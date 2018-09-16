19d6c02181479a3d194b31c0fdbf359db3e82fee
Sonu Pal <sonupalak47@gmail.com>
2018-09-16 21:00:12 +0530

    Login
    Login page redirect problem solved

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,13 +1,11 @@
-
-    function validate()
+document.getElementById("submit").onclick =function validate()
     {
      var username = document.getElementById("username").value;
      var password = document.getElementById("password").value;
      if ( username == "admin" && password == "admin")
         {
           alert(" Login successful !");
-          var url = "https://sonusp.github.io/Admin-Panel/Dashboard/index.html";
-          window.location(url);
+          
           location.href="./Dashboard/index.html"; // Redirecting to other page.
           return false;
         }
